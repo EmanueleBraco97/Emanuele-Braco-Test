dccaab5e2fbde0fd9c4fa503bd40570b3ee65ab8
Emanuele Braco <emanuele.braco@gmail.com>
2023-05-12 11:24:20 +0200

    portfolio-esercizio

---
+++ script.js
@@ -0,0 +1,9 @@
+const mySkillsLink = document.getElementById("mySkills-link");
+const mySkillsMenu = document.getElementById("mySkills-menu");
+
+
+//aggiungo l'evento click al singolo elemento//
+mySkillsLink.addEventListener("click", function () {
+  mySkillsMenu.style.display = "block";
+});
+
